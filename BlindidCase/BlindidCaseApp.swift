6f893e3ef2c3ec33cea44a4aa1fa658ff77ac55b
Eren Korkmaz <eren_korkmaz_@hotmail.com>
2025-05-21 12:27:03 +0300

    Initial Commit

---
+++ BlindidCase/BlindidCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindidCaseApp.swift
+//  BlindidCase
+//
+//  Created by Eren Korkmaz on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlindidCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
